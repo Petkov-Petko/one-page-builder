cdbdecf2d6ddfebbf6c6741c945e309e27bf0536
Petkov-Petko <petkozlatilov1234@gmail.com>
2025-06-04 14:07:08 +0300

    Add BuilderForm component to App layout

--- one-page-builder/src/App.jsx
+++ one-page-builder/src/App.jsx
@@ -1,11 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import BuilderForm from './components/BuilderForm/BuilderForm'
+
 
 function App() {
 
   return (
     <>
-      
-    </>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4">
+            <BuilderForm />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+  </>
   )
 }
 
