c2318af0e85715f617a8bb1280e7ef0edf0d84de
Petkov-Petko <petkozlatilov1234@gmail.com>
2025-06-04 14:07:16 +0300

    Add BuilderForm component with styling

---
+++ one-page-builder/src/components/BuilderForm/BuilderForm.jsx
@@ -0,0 +1,13 @@
+import HeaderEditor from "../HeaderEditor/HeaderEditor"
+import "./BuilderForm.css"
+
+const BuilderForm = () => {
+  return (
+    <div className="form-container">
+        <h1>One</h1>
+        <HeaderEditor />
+    </div>
+  )
+}
+
+export default BuilderForm
