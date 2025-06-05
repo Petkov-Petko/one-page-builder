6bab7437db1104c1496062ecfdd98574c97fa196
Petko Petkov <petkozlatilov1234@gmail.com>
2025-06-05 20:33:39 +0300

    Refactor App component to manage form data state and pass it to BuilderForm and Preview components

--- one-page-builder/src/App.jsx
+++ one-page-builder/src/App.jsx
@@ -2,23 +2,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import BuilderForm from './components/BuilderForm/BuilderForm'
 import Preview from './components/Preview/Preview'
+import { useState } from 'react'
 
 
 function App() {
+  const [formData, setFormData] = useState({
+    lang: "en",
+    title: "",
+    desc: "",
+  });
 
   return (
-    <>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-7">
-            <BuilderForm />
+          <div className="col-md-6">
+          <BuilderForm formData={formData} setFormData={setFormData} />
           </div>
-          <div className="col-md-5">
-            <Preview />
+          <div className="col-md-6">
+          <Preview formData={formData} />
           </div>
         </div>
       </div>
-  </>
   )
 }
 
