a7412e3292ceb6e3fcf5c0abd5b7febf3b7e691b
Petko Petkov <petkozlatilov1234@gmail.com>
2025-07-09 15:28:10 +0300

    removed the row class

--- src/utils/export404.js
+++ src/utils/export404.js
@@ -8,14 +8,12 @@ echo site_header($title, $description);
 ?>
 <div class="container-fluid bg-light py-5 error_page">
     <div class="py-5 text-center">
-        <div class="row justify-content-center">
-            <div class="col-lg-6  ">
+        <div class="justify-content-center">
                 <i class="bi bi-exclamation-circle display-2"></i>
                 <h1 class="display-1">404</h1>
                 <h2 class="mb-4">Page not found</h2>
                 <p class="mb-4">Sorry, the page you are looking for does not exist on our website! Go to the home page.</p>
                 <a class="error_page_btn btn btn-dark text-white rounded-pill py-3 px-5" href="/">Home page</a>
-            </div>
         </div>
     </div>
 </div>
